c125d7e684f3be5248226e0b230c38595fea8fb1
Akesarco <kesarankush@gmail.com>
2018-05-21 10:42:07 -0400

    MongoDB Querying

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -12,21 +12,30 @@ const courseSchema = new mongoose.Schema({
     date: {type: Date, default: Date.now},
     isPublished: Boolean
 });
+const Course = mongoose.model('Course', courseSchema);
 //Models
 //Classes, objects
 //Course,  nodeCourse
 async function  createCourse(){
-    const Course = mongoose.model('Course', courseSchema);
     const course = new Course({
         name: 'Angular.js Course',
         author: 'Ankush',
         tags:['Angular', 'frontend'],    
         isPublished: true
     });
-
     const result = await course.save();
     console.log(result);
 }
 
-createCourse();
+async function getCourses(){
+    const courses = await Course
+    .find({ author: 'Ankush', isPublished: true}) //find in documents were author is Ankush and isPublished is equal  to true
+    .limit(10) // find the results to  10
+    .sort({name: -1}) // sort the name by desc
+    .select({name: 1, tags:1}); // select only name and tags
+    console.log(courses);
+}
+
+getCourses();
+//createCourse();
 
